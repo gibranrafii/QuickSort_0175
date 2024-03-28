6f3ab062135c8d60def54e991ebac10692eaa8d6
gibranrafii <gibranrafi21@gmail.com>
2024-03-28 09:55:07 +0700

    Revisi di bagian prosedur q_short

--- Algoritma 3/Algoritma 3.cpp
+++ Algoritma 3/Algoritma 3.cpp
@@ -57,6 +57,12 @@ void q_short(int low, int high)
             cmp_count++;
         }
         cmp_count++;
+        while ((arr[j] > pivot) && (j >= low))
+        {
+            j--;
+            cmp_count++;
+        }
+        cmp_count++;
         if (1 < j)
         {
             swap(i, j);
