494a751b232be59758e740de853c7c27edf4fd6a
gibranrafii <gibranrafi21@gmail.com>
2024-03-28 09:14:11 +0700

    Membuat looping for

--- Algoritma 3/Algoritma 3.cpp
+++ Algoritma 3/Algoritma 3.cpp
@@ -20,6 +20,12 @@ void input() {
     cout << "\n-------------------" << endl;
     cout << "\nEnter Array Element" << endl;
     cout << "\n-------------------" << endl;
+
+    for (int i = 0; 1 < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
 
 int main() 
