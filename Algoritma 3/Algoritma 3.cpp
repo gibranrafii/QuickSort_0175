f83696b089a21fc9bf85ad87bd009b0b1d6aa3c1
gibranrafii <gibranrafi21@gmail.com>
2024-03-28 09:07:28 +0700

    Menyelesaikan looping while

--- Algoritma 3/Algoritma 3.cpp
+++ Algoritma 3/Algoritma 3.cpp
@@ -9,7 +9,12 @@ int n;
 void input() {
     while (true)
     {
-       
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else cout << "\nMaksimum panjang array adalah 20" << endl;
     }
 
 }
